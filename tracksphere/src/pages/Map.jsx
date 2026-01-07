23471dd4c4ee7469387963571f36720c12cc0fec
Redge07 <148580943+Redge07@users.noreply.github.com>
2026-01-07 17:16:20 +0100

    Change header text from 'Marre' to 'Marre2'

--- tracksphere/src/pages/Map.jsx
+++ tracksphere/src/pages/Map.jsx
@@ -4,7 +4,7 @@ const Map = () => {
   const [cpt, setCpt] = useState(1);
   return (
     <div>
-      <h1>Marre</h1>
+      <h1>Marre2</h1>
       <p>Bonsoir</p> <p>{cpt}</p>{" "}
       <button onClick={() => setCpt((prev) => prev + 1)}>+1</button>
     </div>
